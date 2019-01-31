2cb617576f4e4481c16f6ef5ff4eef84048a6a96
luc <luctunechi45@gmail.com>
2019-01-31 15:52:39 +0200

    add boolean to javascript

---
+++ basics/boolean.js
@@ -0,0 +1,13 @@
+// === equality operator
+// !== not equal operator
+// < less than operator
+// > greater than operator
+// <= less than or equal to operator
+// >= greater than or equal to operator
+
+let age = 24
+
+let isChild = age <= 7
+let isSenior = age >= 65
+console.log(isChild)
+console.log(isSenior)
