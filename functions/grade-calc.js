c0ae07f8b16ae9b6d2a72f5183b422686a4f623b
luc <luctunechi45@gmail.com>
2019-02-03 18:05:37 +0200

    add grade calculation

---
+++ functions/grade-calc.js
@@ -0,0 +1,24 @@
+// student, score, total possible score
+// 15/20 => You got a grace C
+// A 90-100, B 80-89, C 78-79, D 60-69, F 0-59
+
+let gradeCal = (score, totalScore) => {
+  let percent = (score / totalScore) * 100
+  let letterGrade = ''
+  if (percent >= 90) {
+   letterGrade = 'A'
+  } else if (percent >= 80) {
+    letterGrade = 'B'
+  } else if (percent >= 78) {
+    letterGrade = 'C'
+  } else if (percent >= 60) {
+    letterGrade = 'D'
+  } else {
+    letterGrade = 'F'
+  }
+
+    return `You got a (${letterGrade}) (${percent})!`
+}
+
+let result = gradeCal(16, 20)
+console.log(result)
