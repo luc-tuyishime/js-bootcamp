112ff4f5e8465d5c536d0eb35a6da711b8c1d53d
luc <luctunechi45@gmail.com>
2019-02-10 11:09:01 +0200

    add Todos list Array

--- arrays/notes.js
+++ arrays/notes.js
@@ -1,8 +1,3 @@
 const notes = ['note 1', 'notes 2', 'note 3']
 console.log(notes)
 console.log(notes[0])
-
-const todos = ['drink', 'pray', 'read', 'dance', 'teach']
-console.log(`You have ${todos.length} todos`)
-console.log(`Todo: ${todos[0]}`)
-console.log(`Todo: ${todos[todos.length - 1]}`) // Get the last item
---
+++ arrays/todos.js
@@ -0,0 +1,4 @@
+const todos = ['drink', 'pray', 'read', 'dance', 'teach']
+console.log(`You have ${todos.length} todos`)
+console.log(`Todo: ${todos[0]}`)
+console.log(`Todo: ${todos[todos.length - 1]}`) // Get the last item
