2c5a5ce16f4220b6bd04d34b327ad3902d8b4fae
luc <luctunechi45@gmail.com>
2019-02-11 12:58:31 +0200

    add more arrays

--- arrays/notes.js
+++ arrays/notes.js
@@ -1,3 +1,12 @@
 const notes = ['note 1', 'notes 2', 'note 3']
+
+// console.log(notes.pop())
+// notes.push('New note')
+// console.log(notes.shift())
+// console.log(notes.unshift('My first note'))
+
+// notes.splice(1, 0, 'This is the second item') // add item to the list on the second list(1) or do (1, 1) to remove and replace with new string
+
+notes[2] = 'This is now the new note 3'
+
 console.log(notes)
-console.log(notes[0])
--- arrays/todos.js
+++ arrays/todos.js
@@ -1,4 +1,13 @@
 const todos = ['drink', 'pray', 'read', 'dance', 'teach']
-console.log(`You have ${todos.length} todos`)
-console.log(`Todo: ${todos[0]}`)
-console.log(`Todo: ${todos[todos.length - 1]}`) // Get the last item
+// console.log(`You have ${todos.length} todos`)
+// console.log(`Todo: ${todos[0]}`)
+// console.log(`Todo: ${todos[todos.length - 1]}`) // Get the last item
+
+console.log(todos);
+todos.splice(2, 1); // Go to the third item and remove it
+console.log(todos);
+
+todos.splice(4, 0, 'the new item is added') // Go to the last item and change the property
+console.log(todos)
+todos.shift(0) // Remove the first item
+console.log(todos)
