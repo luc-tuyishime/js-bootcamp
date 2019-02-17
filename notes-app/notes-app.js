b46a226e1f7086e0a2e8df61a46d22c110667e17
luc <luctunechi45@gmail.com>
2019-02-17 20:35:51 +0200

    interact with DOM (todos manipulation)

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -9,3 +9,8 @@ ps.forEach((p) => { // iterate through each
   // p.remove() // remove on by one
   console.log(p.textContent)
 })
+
+// Add a new element
+const newParagraph = document.createElement('p')
+newParagraph.textContent = 'This is the new element from Javascript'
+document.querySelector('body').append(newParagraph) 
--- todos-app/todos-app.js
+++ todos-app/todos-app.js
@@ -1,6 +1,44 @@
-const ps = document.querySelectorAll('p')
-ps.forEach((p) => {
-  if(p.textContent.includes('the')){
-    p.remove()
+const todos = [
+  {
+    text: 'Order a cat',
+    complited: true
+  },
+  {
+    text: 'Play football',
+    complited: false
+  },
+  {
+    text: 'Drink some wine',
+    complited: true
+   },
+  {
+    text: 'Go to school everyday',
+    complited: true
+  },
+  {
+    text: 'Buy food',
+    complited: false
   }
+]
+
+// const ps = document.querySelectorAll('p')
+// ps.forEach((p) => {
+//   if(p.textContent.includes('the')){
+//     p.remove()
+//   }
+// })
+
+const incompleteTodos = todos.filter((todo) => { // filter incomplet Todos
+  return !todo.complited
+})
+
+const summary = document.createElement('h2')
+summary.textContent = `you have ${incompleteTodos.length} todos left`
+document.querySelector('body').appendChild(summary)
+
+
+todos.forEach((todo) => {
+  const p  = document.createElement('p')
+  p.textContent = todo.text
+  document.querySelector('body').appendChild(p)
 })
